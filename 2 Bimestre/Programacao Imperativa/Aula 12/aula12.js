930f58e6bce2bc58e38fa295c02b15783cc7b701
Knowther <parentejohnny9@gmail.com>
2021-11-17 22:44:31 -0300

    mudança aula 12 prog emp

--- 2 Bimestre/Programacao Imperativa/Aula 12/aula12.js
+++ 2 Bimestre/Programacao Imperativa/Aula 12/aula12.js
@@ -2,24 +2,19 @@
 const alicia = [30, 30, 30];
 const bob = [30,30, 48];
 
-findWinner(alicia, bob);
+console.log(findWinner(alicia, bob));
 
 function findWinner(a=[], b=[]){
     let aliciaPoints =0;
     let bobPoints =0;
-    let isAliciaWin;
     for(let i =0; i < a.length; i++){
         if(a[i] > b[i]){
-            aliciaPoints = aliciaPoints + 1;
+            aliciaPoints++;
         }
         else if(a[i] < b[i]){
-            bobPoints = bobPoints + 1;
+            bobPoints++;
         }else{
         }
     }
-    if(aliciaPoints > bobPoints){
-        console.log(`Alice ganhou com ${aliciaPoints} pontos`);
-    }else{
-        console.log(`Bob ganhou com ${bobPoints} pontos`);
-    }
+    return aliciaPoints > bobPoints ? `Alicia ganhou com ${aliciaPoints} pontos`: `Bob ganhou com ${bobPoints} pontos`;
 }
