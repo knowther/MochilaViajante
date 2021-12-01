f8b031f2b159160df5910ff40cd59d6bd381614f
Knowther <parentejohnny9@gmail.com>
2021-12-01 08:08:48 -0300

    mais ajustes na mesa de array avançado

--- 2 Bimestre/Programacao Imperativa/Aula 17/mesa.js
+++ 2 Bimestre/Programacao Imperativa/Aula 17/mesa.js
@@ -1,31 +1,29 @@
 let numeros = [1, 2, 3, 4];
 
-function divSum(array){
-
-    let total = array.reduce((acumulador, item) => {
-        // return acumulador + item
-       return acumulador + item
-    })
-   let numArray = array.map(numero => {
-        return numero / total
-     })
-
-     return numArray;
+function divSum(array) {
+  let total = array.reduce((acumulador, item) => {
+    return acumulador + item;
+  });
+  let numArray = array.map((numero) => {
+    return numero / total;
+  });
+  return numArray;
 }
 
+let arrayPalavras = ["Caneta", "Casa", "Manjericão"];
 
+function stringCounter(array, num) {
+  let array1 = array.filter((string) => {
+    return string.length > num;
+  });
 
-let arrayPalavras = ['Caneta', 'Casa', 'Manjericão'];
+  return array1;
+}
 
-    function test (array, num){
-       let array1 = array.filter(string => {
-            return string.length > num;
-         })
+let animais = ["Gato", "Cachorro", "Cavalo"];
 
-         return array1;
-    }
+console.log(stringCounter(arrayPalavras, 5));
 
-
-   
-    console.log(test(arrayPalavras, 5))
-// console.log(divSum(numeros))
+let stringAnimal = animais.forEach((animal) => {
+  console.log(`O animal é ${animal}`);
+});
