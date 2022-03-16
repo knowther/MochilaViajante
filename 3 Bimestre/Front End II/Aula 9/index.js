3cceab623283656190297577671a702184ff000a
Knowther <johnnywesley.contato@outlook.com>
2022-03-15 23:51:36 -0300

    aula 9 callbacks ok

---
+++ 3 Bimestre/Front End II/Aula 9/index.js
@@ -0,0 +1,8 @@
+const soma = require("./soma");
+const subtrair = require("./subtrair");
+const multiplicar = require("./multiplicar");
+const dividir = require("./divisao");
+
+let calculadora = (valor1, valor2, operacao) => console.log(operacao(valor1, valor2)); 
+
+calculadora(5, 2, multiplicar);
---
+++ 3 Bimestre/Front End II/Aula 9/multiplicar.js
@@ -0,0 +1,3 @@
+let multiplicar = (valor1, valor2) => valor1 * valor2;
+
+module.exports = multiplicar;
