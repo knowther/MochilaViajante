e1c84c0b19c7b601df6972845627ae874ec1bcac
Knowther <johnnywesley.contato@outlook.com>
2022-06-13 12:41:13 -0300

    projeto inicial RN

---
+++ DIO/Avanade Decola Tech 3rd edition/Module 4 - React Native/Projetos/dio/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+const App = () => {
+  return (
+    <View>
+      <Text>Hello word</Text>
+    </View>
+  );
+};
+
+export default App;
