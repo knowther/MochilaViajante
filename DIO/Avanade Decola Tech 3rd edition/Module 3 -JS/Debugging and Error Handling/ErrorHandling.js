7e514835d36531523741b890e2471bd11d1ccb00
Knowther <johnnywesley.contato@outlook.com>
2022-06-18 17:07:20 -0300

    desafio de acordo com os requisitor do exercício

--- DIO/Avanade Decola Tech 3rd edition/Module 3 -JS/Debugging and Error Handling/ErrorHandling.js
+++ DIO/Avanade Decola Tech 3rd edition/Module 3 -JS/Debugging and Error Handling/ErrorHandling.js
@@ -1,16 +1,31 @@
-function readArray(array) {
-  console.log(typeof array);
-  readArray.arguments.length == 0
-    ? console.log(new ReferenceError("Sem Argumentos"))
-    : console.log("Nº de argumentos corretos.");
-  typeof array !== "object"
-    ? console.log(new TypeError("Parâmetro não é um objeto."))
-    : console.log("Parâmetro aceito");
-  array.forEach((el, i) => {
-    typeof el !== "number"
-      ? console.log(new TypeError(`"Índice ${i} do array não é um número"`))
-      : console.log("Todos índices do Array são compatíveis.");
-  });
+function readArray(array, numElements) {
+  try {
+    if (readArray.arguments.length == 0)
+      throw new ReferenceError("Sem parâmetros");
+    if (typeof array !== "object")
+      throw new TypeError("Parâmetro 1 da função não é um objeto.");
+    if (typeof numElements !== "number")
+      throw ReferenceError(
+        "Segundo parâmetro da função deve ser do tipo numérico."
+      );
+    array.forEach((el, i) => {
+      if (typeof el !== "number")
+        throw new TypeError(`"Índice ${i} do array não é um número"`);
+    });
+    if (array.length !== numElements)
+      throw new RangeError("Número de índices do array incompatível");
+  } catch (error) {
+    if (error instanceof ReferenceError) {
+      console.log("Erro do tipo ReferenceError!");
+      console.log(error.message);
+    } else if (error instanceof TypeError) {
+      console.log("Erro do tipo TypeError!");
+      console.log(error.message);
+    } else if (error instanceof RangeError) {
+      console.log("Erro do tipo RangeError!");
+      console.log(error.message);
+    }
+  }
 }
 
-readArray([1, 2, "B", 3]);
+readArray();
