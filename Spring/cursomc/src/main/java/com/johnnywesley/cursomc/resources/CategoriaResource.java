0e165fee0de79edebc922f4f14790183c75d2286
Knowther <johnnywesley.contato@outlook.com>
2022-09-10 13:29:12 -0300

    classe categoria funcional, primeira classe de domínio

--- Spring/cursomc/src/main/java/com/johnnywesley/cursomc/resources/CategoriaResource.java
+++ Spring/cursomc/src/main/java/com/johnnywesley/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package com.johnnywesley.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.johnnywesley.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcional";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 	
 }
