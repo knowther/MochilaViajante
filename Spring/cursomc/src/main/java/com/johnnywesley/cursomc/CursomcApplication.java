83d6367b3b422bbc308eef35867c3f71e99b57d9
Knowther <johnnywesley.contato@outlook.com>
2022-09-10 20:01:26 -0300

    instanciando categorias e inserindo-as na base de dados

--- Spring/cursomc/src/main/java/com/johnnywesley/cursomc/CursomcApplication.java
+++ Spring/cursomc/src/main/java/com/johnnywesley/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.johnnywesley.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.johnnywesley.cursomc.domain.Categoria;
+import com.johnnywesley.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Informárica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
