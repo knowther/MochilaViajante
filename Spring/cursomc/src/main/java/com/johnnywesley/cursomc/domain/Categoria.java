6d92fd263ef62d9089529f1a6b34f6f9faf8b6d7
Knowther <johnnywesley.contato@outlook.com>
2022-09-10 19:14:54 -0300

    anotações JPA, banco de dados de teste H2, criação automática da base de dados a partir dos objetos

--- Spring/cursomc/src/main/java/com/johnnywesley/cursomc/domain/Categoria.java
+++ Spring/cursomc/src/main/java/com/johnnywesley/cursomc/domain/Categoria.java
@@ -3,8 +3,16 @@ package com.johnnywesley.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
